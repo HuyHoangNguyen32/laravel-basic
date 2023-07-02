cf5a10f0d17f49d6c96a4fd3c952012b96191396
HuyHoangNguyen32 <92217902+HuyHoangNguyen32@users.noreply.github.com>
2023-07-02 23:30:12 +0900

    Upload all files

---
+++ resources/views/tests/test.blade.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Test</title>
+</head>
+<body>
+
+<!-- @foreach($data as $value)
+
+<ul>
+  <li></li>
+  <li></li>
+  <li></li>
+</ul>
+
+@endforeach -->
+
+  <h1>Test</h1>
+  <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit consequatur, perspiciatis repellendus, corrupti necessitatibus cumque debitis quisquam, totam commodi tempore id inventore. Cupiditate nemo eaque voluptates maxime ducimus dignissimos. Voluptas, ipsa iure? Iste fugit eius quidem! Dicta consequuntur pariatur voluptatibus illum deleniti, labore soluta maxime sit, voluptatum nemo molestiae adipisci.</p>
+
+</body>
+</html>
